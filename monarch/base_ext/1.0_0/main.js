c0572fde7bad41a51f450b0efe9008933c1882f9
Dan Whitcomb <danrwhitcomb@gmail.com>
2016-05-17 14:09:31 -0400

    Gave base mda download permissions

--- monarch/base_ext/1.0_0/main.js
+++ monarch/base_ext/1.0_0/main.js
@@ -6,3 +6,11 @@ function updateWebviews() {
 
 onload = updateWebviews;
 window.onresize = updateWebviews;
+
+//Give apps additional permissions
+var webview = document.querySelector("webview");
+webview.addEventListener('permissionrequest', function(e) {
+  if (e.permission === 'download') {
+    e.request.allow();
+  }
+});
